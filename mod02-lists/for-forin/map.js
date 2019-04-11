b791b333c6413685a1f85e2817fa9762418d4cd3
Islan Rodrigues <jose-islan52@hotmail.com>
2019-04-11 18:26:14 -0300

    Criação e uso de uma função map() própria (meuMap)

--- mod02-lists/for-forin/map.js
+++ mod02-lists/for-forin/map.js
@@ -1,5 +1,16 @@
 const service = require('./service');
 
+Array.prototype.meuMap = function(callback) {
+    const arrayResultados = [];
+
+    for(let index = 0; index < this.length; index++) {
+        arrayResultados.push(callback(this[index], index));
+    }
+
+    return arrayResultados;
+}
+
+
 async function main() {
 
     try {
@@ -10,13 +21,20 @@ async function main() {
         // result.results.forEach(function(item) {
         //     names.push(item.name);
         // });
-
         // console.log("Names: ", names);
         
-        //Trabalhando com o método map()
-        const names = result.results.map(pessoa => pessoa.name);
-        console.log("Names: ", names);
 
+        //Trabalhando com o método map()
+        // const names = result.results.map(pessoa => pessoa.name);
+        // console.log("Names: ", names);
+
+
+        //Trabalhando com o método meuMap criado
+        const names = result.results.meuMap((pessoa, indice) => {
+            return `${indice} - ${pessoa.name}`;
+        });
+
+        console.log("Names: ", names);
 
     } catch (error) {
         console.error("ERROR: ", error);
