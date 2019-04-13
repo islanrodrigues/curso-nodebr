6a74e465ddc3d7f6c38f3b540fd8235e3ec8bbff
Islan Rodrigues <jose-islan52@hotmail.com>
2019-04-13 18:46:43 -0300

    Criação e uso de uma função reduce() própria (meuReduce)

--- mod02-lists/for-forin/reduce.js
+++ mod02-lists/for-forin/reduce.js
@@ -1,17 +1,36 @@
 const {obterPessoas} = require('./service');
 
+Array.prototype.meuReduce = function (callback, initialValue) {
+    let valorFinal = typeof initialValue !== undefined ? initialValue : this[0];
+
+    for (let index = 0; index <  this.length; index++) {
+        valorFinal = callback(valorFinal, this[index], this);
+    }
+
+    return valorFinal;
+}
+
+
 async function main() {
 
     try {
         const {results} = await obterPessoas('a');
         const pesos = results.map(item => parseInt(item.height));
 
-        const total = pesos.reduce((anterior, proximo) => {
+        // const total = pesos.reduce((anterior, proximo) => {
+        //     return anterior + proximo;
+        // }, 0);
+
+        // console.log("Pesos: ", pesos);
+        // console.log("Total: ", total);
+
+
+        const total = pesos.meuReduce((anterior, proximo, minhaLista) => {
             return anterior + proximo;
         }, 0);
 
         console.log("Pesos: ", pesos);
-        console.log("Total: ", total);
+        console.log("soma Pesos: ", total);
          
     } catch (error) {
         console.error("ERROR: ", error);
